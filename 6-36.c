1a466af4eb5452364a815872b16cfa88da17b408
hihi_1207 <48911226+hihihi1207@users.noreply.github.com>
2025-04-15 22:23:09 +0900

    a

---
+++ 6-36.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(void){
+    char blood;
+    printf("A형 : A 또는 a\n");
+    printf("B형 : B 또는 b\n");
+    printf("AB형 :  C 또는 c\n");
+    printf("O형 : O 또는 o\n");
+    printf("%c",&blood, sizeof(blood)); 
+
+    if(blood == 'A' || blood == 'a'){
+        printf("A형 입니다.\n");
+        printf("부드럽고 섬세한 성향\n");
+    }
+    else if(blood == 'C' || blood == 'c'){
+        printf("AB형 입니다.\n");
+        printf("예술적인 감각을 가진 성향\n");
+    }
+    else if(blood == 'O' || blood == 'o'){
+        printf("O형입니다.\n");
+        printf("활발하고 쾌활한 성향\n");
+    }
+    else if(blood == 'B' || blood == 'b'){
+        printf("B형입니다.\n");
+        printf("주위 변화에 민감한 성향\n");
+    }
+    else{
+        printf("허용하지 않는 문자입니다.\n");
+        printf("대소 문자 구별 없이 A,B,C,O만 허용됩니다.\n");
+        printf("프로그램을 다시 시작하세요.\n");
+    }
+}
--- 구구단(for문).c
+++ 구구단(for문).c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-	for (int i = 1; i <= 9; i++) {
-		for (int a = 0; a <= 9; a++) {
-			printf("%d x %d = %d \n", i, a, i * a);
-		}
+	for (int i = 1; i <= 9; i++)// i = 구구단 앞부분에 들어가는 수    for문 해석 i=1이고 안에있는 코드를 실행하고 i에 1을 더한다 i가 9보다 작거나 같을때까지 반복
+		{
+		for (int a = 0; a <= 9; a++) // a = 구구단 뒷부분에 들어가는 수        for문 해석  a는 0이고 코드를 실행하고 a에 1을더한다  a가 9보다 작거나 같을때까지 반복
+		{
+			printf("%d x %d = %d \n", i, a, i * a);    
+		} // 이부분에서 2번째 for문 끝나고 첫번째 for문으로 올라감
 	}
 }
