1b82ca31a932cc12fccaee86401e78ef1f7b8e8f
hihi_1207 <48911226+hihihi1207@users.noreply.github.com>
2025-04-15 21:38:49 +0900

    first

---
+++ 구구단(for문).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+	for (int i = 1; i <= 9; i++) {
+		for (int a = 0; a <= 9; a++) {
+			printf("%d x %d = %d \n", i, a, i * a);
+		}
+	}
+	system("pause");
+	return 0;
+}
