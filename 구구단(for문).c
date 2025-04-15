c1ea09d4bca0ee0f6ef5b6ddd31197052743c385
hihi_1207 <48911226+hihihi1207@users.noreply.github.com>
2025-04-15 21:41:21 +0900

    Update 구구단(for문).c

--- 구구단(for문).c
+++ 구구단(for문).c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-
 	for (int i = 1; i <= 9; i++) {
 		for (int a = 0; a <= 9; a++) {
 			printf("%d x %d = %d \n", i, a, i * a);
 		}
 	}
-	system("pause");
-	return 0;
 }
